2e1ba78d03d07eb235d2adf6d6bd6484708cd4fd
gwakjeaha <kjh19920718@gmail.com>
2023-02-07 18:07:36 +0900

    feat: 북마크 엔티티 생성(#6)

---
+++ src/main/java/com/devtraces/arterest/domain/bookmark/Bookmark.java
@@ -0,0 +1,57 @@
+package com.devtraces.arterest.domain.bookmark;
+
+import com.devtraces.arterest.common.domain.BaseEntity;
+import com.devtraces.arterest.domain.feed.Feed;
+import com.devtraces.arterest.domain.user.User;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.envers.AuditOverride;
+
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AuditOverride(forClass = BaseEntity.class)
+@Table(name = "bookmark")
+@Entity
+public class Bookmark {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "bookmark_id")
+	private Long id;
+
+	private Long feedId;
+	private Long userId;
+
+	private LocalDateTime createdAt;
+
+	// N:1 mapping with User
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	@ToString.Exclude
+	private User user;
+
+	// N:1 mapping with Feed
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "feed_id")
+	@ToString.Exclude
+	private Feed feed;
+
+}
