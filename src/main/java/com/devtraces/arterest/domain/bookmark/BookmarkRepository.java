f1aee3fbcee8d0137ac120f2e4b4f3dcff9e7b5c
gwakjeaha <kjh19920718@gmail.com>
2023-02-07 18:13:24 +0900

    feat: 북마크 Repository 추가(#6)

---
+++ src/main/java/com/devtraces/arterest/domain/bookmark/BookmarkRepository.java
@@ -0,0 +1,7 @@
+package com.devtraces.arterest.domain.bookmark;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+}
