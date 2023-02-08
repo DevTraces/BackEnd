3769b3145a760b4b33d34ecabd8c4c974a6a3a66
soeunkk <luckyber1@naver.com>
2023-02-09 08:46:10 +0900

    test: 로그인 Service 테스트 (#5)

---
+++ src/test/java/com/devtraces/arterest/service/user/AuthServiceTest.java
@@ -0,0 +1,88 @@
+package com.devtraces.arterest.service.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import com.devtraces.arterest.common.exception.BaseException;
+import com.devtraces.arterest.common.jwt.JwtProvider;
+import com.devtraces.arterest.common.jwt.dto.TokenDto;
+import com.devtraces.arterest.common.redis.service.RedisService;
+import com.devtraces.arterest.domain.user.User;
+import com.devtraces.arterest.domain.user.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+	@Mock
+	private JwtProvider jwtProvider;
+	@Mock
+	private RedisService redisService;
+	@Mock
+	private UserRepository userRepository;
+
+	@InjectMocks
+	private AuthService authService;
+
+	@Test
+	void testSignInAndGenerateJwtToken() {
+		User mockUser = User.builder()
+			.id(1L)
+			.email("email@gmail.com")
+			.password("encodingPassword")
+			.build();
+		TokenDto mockTokenDto = TokenDto.builder()
+			.accessToken("access-token")
+			.refreshToken("refresh-token")
+			.build();
+		given(userRepository.findByEmail(anyString()))
+			.willReturn(Optional.of(mockUser));
+		given(passwordEncoder.matches(anyString(), anyString()))
+			.willReturn(true);
+		given(jwtProvider.generateAccessTokenAndRefreshToken(anyLong()))
+			.willReturn(mockTokenDto);
+
+		authService.signInAndGenerateJwtToken("email@gmail.com", "pw");
+	}
+
+	// 조회되지 않는 이메일인 경우
+	@Test
+	void testSignInAndGenerateJwtTokenByWrongEmail() {
+		given(userRepository.findByEmail(anyString()))
+			.willReturn(Optional.empty());
+
+		BaseException exception = assertThrows(BaseException.class,
+			() -> authService.signInAndGenerateJwtToken("wrong@gmail.com", "pw"));
+
+		assertEquals(BaseException.WRONG_EMAIL_OR_PASSWORD, exception);
+	}
+
+	// 비밀번호가 불일치하는 경우
+	@Test
+	void testSignInAndGenerateJwtTokenByWrongPassword() {
+		User mockUser = User.builder()
+			.id(1L)
+			.email("email@gmail.com")
+			.password("encodingPassword")
+			.build();
+		given(userRepository.findByEmail(anyString()))
+			.willReturn(Optional.of(mockUser));
+		given(passwordEncoder.matches(anyString(), anyString()))
+			.willReturn(false);
+
+		BaseException exception = assertThrows(BaseException.class,
+			() -> authService.signInAndGenerateJwtToken("email@gmail.com", "wrongPw"));
+
+		assertEquals(BaseException.WRONG_EMAIL_OR_PASSWORD, exception);
+	}
+}
