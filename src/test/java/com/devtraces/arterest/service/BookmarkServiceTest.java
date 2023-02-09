194ee7ec21ea9c6697d93be57f2b667de8e090e1
gwakjeaha <kjh19920718@gmail.com>
2023-02-09 20:00:38 +0900

    test: 북마크 생성 로직 변경에 따른 테스트 코드 수정 (#6)

--- src/test/java/com/devtraces/arterest/service/BookmarkServiceTest.java
+++ src/test/java/com/devtraces/arterest/service/BookmarkServiceTest.java
@@ -1,6 +1,9 @@
 package com.devtraces.arterest.service;
 
+import static com.devtraces.arterest.common.exception.ErrorCode.FEED_NOT_FOUND;
+import static com.devtraces.arterest.common.exception.ErrorCode.USER_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -8,12 +11,17 @@ import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.devtraces.arterest.common.exception.BaseException;
 import com.devtraces.arterest.domain.bookmark.Bookmark;
 import com.devtraces.arterest.domain.bookmark.BookmarkRepository;
 import com.devtraces.arterest.domain.feed.Feed;
+import com.devtraces.arterest.domain.feed.FeedRepository;
+import com.devtraces.arterest.domain.user.User;
+import com.devtraces.arterest.domain.user.UserRepository;
 import com.devtraces.arterest.dto.GetBookmarkListResponse;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -33,6 +41,12 @@ class BookmarkServiceTest {
 
 	@Mock
 	private BookmarkRepository bookmarkRepository;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private FeedRepository feedRepository;
 
 	@Test
 	void testGetBookmarkList() throws Exception{
@@ -62,6 +76,16 @@ class BookmarkServiceTest {
 	@Test
 	void testCreateBookmark() throws Exception{
 		// given
+		given(userRepository.findById(anyLong()))
+			.willReturn(Optional.ofNullable(User.builder()
+				.id(1L)
+				.build()));
+
+		given(feedRepository.findById(anyLong()))
+			.willReturn(Optional.ofNullable(Feed.builder()
+				.id(2L)
+				.build()));
+
 		ArgumentCaptor<Bookmark> captor = ArgumentCaptor.forClass(Bookmark.class);
 
 		// when
@@ -71,6 +95,34 @@ class BookmarkServiceTest {
 		verify(bookmarkRepository, times(1)).save(captor.capture());
 		assertEquals(1L, captor.getValue().getUser().getId());
 		assertEquals(2L, captor.getValue().getFeed().getId());
+	}
+
+	@Test
+	void testNotUserFoundInCreateBookmark() throws Exception{
+		//given
+
+		//when
+		BaseException exception = assertThrows(BaseException.class,
+			() -> bookmarkService.createBookmark(1L, 2L));
+
+		//then
+		assertEquals(new BaseException(USER_NOT_FOUND).getErrorCode(), exception.getErrorCode());
+	}
+
+	@Test
+	void testNotFeedFoundInCreateBookmark() throws Exception{
+		//given
+		given(userRepository.findById(anyLong()))
+			.willReturn(Optional.ofNullable(User.builder()
+				.id(1L)
+				.build()));
+
+		//when
+		BaseException exception = assertThrows(BaseException.class,
+			() -> bookmarkService.createBookmark(1L, 2L));
+
+		//then
+		assertEquals(new BaseException(FEED_NOT_FOUND).getErrorCode(), exception.getErrorCode());
 	}
 
 	@Test
