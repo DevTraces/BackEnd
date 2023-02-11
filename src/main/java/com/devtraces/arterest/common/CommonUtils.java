0f8999af50e0c861a3202acfe9f33d0f87e2e016
Dongvin,Park <dongvin99@naver.com>
2023-02-11 22:03:17 +0900

    feat: 해시태그 개수 제한 상수 추가 (#24)

--- src/main/java/com/devtraces/arterest/common/CommonUtils.java
+++ src/main/java/com/devtraces/arterest/common/CommonUtils.java
@@ -3,5 +3,6 @@ package com.devtraces.arterest.common;
 public class CommonUtils {
 
     public static final int CONTENT_LENGTH_LIMIT = 1000;
+    public static final int HASHTAG_COUNT_LIMIT = 10;
 
 }
