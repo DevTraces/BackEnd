04f4c10524a0ded98cf11eeeb8c8fb5995dd4964
Dongvin,Park <dongvin99@naver.com>
2023-02-12 11:19:45 +0900

    fix: 게시물 읽기/삭제 기능에 레디스 로직 추가 완료. (#24)

--- src/main/java/com/devtraces/arterest/service/feed/FeedService.java
+++ src/main/java/com/devtraces/arterest/service/feed/FeedService.java
@@ -7,6 +7,7 @@ import com.devtraces.arterest.domain.feed.Feed;
 import com.devtraces.arterest.domain.feed.FeedRepository;
 import com.devtraces.arterest.domain.like.LikeRepository;
 import com.devtraces.arterest.domain.like.Likes;
+import com.devtraces.arterest.domain.likecache.LikeNumberCacheRepository;
 import com.devtraces.arterest.domain.reply.Reply;
 import com.devtraces.arterest.domain.reply.ReplyRepository;
 import com.devtraces.arterest.domain.rereply.Rereply;
@@ -29,26 +30,28 @@ public class FeedService {
     private final RereplyRepository rereplyRepository;
     private final LikeRepository likeRepository;
     private final BookmarkRepository bookmarkRepository;
+    private final LikeNumberCacheRepository likeNumberCacheRepository;
 
     @Transactional(readOnly = true)
     public List<FeedResponse> getFeedResponseList(Long userId, PageRequest pageRequest){
         // 요청한 사용자가 좋아요를 누른 피드들의 주키 아이디 번호들을 먼저 불러온다.
-        // 레디스가 셋팅된 후에는 레디스에 캐시된 좋아요 게시물 주키 번호 리스트를 먼저 보고, 없을 때만 DB 보게 만든다.
         Set<Long> likedFeedSet = likeRepository.findAllByUserId(userId)
             .stream().map(Likes::getFeedId).collect(Collectors.toSet());
 
+        // 요청한 사용자가 북마크 했던 피드들의 주키 아이디 번호들도 불러온다.
         Set<Long> bookmarkedFeedSet = bookmarkRepository.findAllByUserId(userId)
             .stream().map(bookmark -> bookmark.getFeed().getId()).collect(Collectors.toSet());
 
         // 피드 별 좋아요 개수는 레디스를 먼저 보게 만들고, 그게 불가능 할때만 Like 테이블에서 찾도록 한다.
-        // 현재 레디스 셋팅이 완료되지 않았으므로 DB에서 좋아요 개수를 찾아내게 만든다.
         return feedRepository.findAllByUserId(userId, pageRequest).stream().map(
-            feed -> FeedResponse.from(
-                feed, likedFeedSet, likeRepository.countByFeedId(feed.getId()), bookmarkedFeedSet
-            )).collect(Collectors.toList());
+            feed -> {
+                Long likeNumber = likeNumberCacheRepository.getFeedLikeNumber(feed.getId());
+                if(likeNumber == null) likeNumber = likeRepository.countByFeedId(feed.getId());
+                return FeedResponse.from(feed, likedFeedSet, likeNumber, bookmarkedFeedSet);
+            }
+        ).collect(Collectors.toList());
     }
 
-    // 레디스 추가 후 좋아요 개수를 레디스에서 먼저 얻어오도록 수정하는 것 필요.
     @Transactional(readOnly = true)
     public FeedResponse getOneFeed(Long userId, Long feedId){
         Set<Long> likedFeedSet = likeRepository.findAllByUserId(userId)
@@ -59,9 +62,13 @@ public class FeedService {
 
         Feed feed = feedRepository.findById(feedId).orElseThrow(() -> BaseException.FEED_NOT_FOUND);
 
-        //커스텀 예외 사용방법 확정 후 수정 필요.
+        Long likeNumber = likeNumberCacheRepository.getFeedLikeNumber(feedId);
+        if(likeNumber == null) {
+            likeNumber = likeRepository.countByFeedId(feedId);
+        }
+
         return FeedResponse.from(
-            feed, likedFeedSet, likeRepository.countByFeedId(feedId), bookmarkedFeedSet
+            feed, likedFeedSet, likeNumber, bookmarkedFeedSet
         );
     }
 
@@ -74,6 +81,9 @@ public class FeedService {
         if(!Objects.equals(feed.getUser().getId(), userId)){
             throw BaseException.USER_INFO_NOT_MATCH;
         }
+
+        // 레디스에서 좋아요 개수 기록한 키-밸류 쌍 삭제.
+        likeNumberCacheRepository.deleteLikeNumberInfo(feedId);
 
         // 좋아요 테이블에서 정보 모두 삭제
         likeRepository.deleteAllByFeedId(feedId);
