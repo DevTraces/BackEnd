e20d2b53d5c4979fadb9462740c803ea4e890f6d
heyazoo1007 <heyazoo1007@gmail.com>
2023-02-14 21:38:11 +0900

    fix: CORS 에러 해결위해서 SpringConfiguration에서 CORS 설정 로직 추가 (#37)

--- src/main/java/com/devtraces/arterest/configuration/SecurityConfiguration.java
+++ src/main/java/com/devtraces/arterest/configuration/SecurityConfiguration.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -40,6 +45,20 @@ public class SecurityConfiguration {
 		http
 			.exceptionHandling()
 			.authenticationEntryPoint(new JwtAuthenticationEntryPoint());
+
+		http.cors();
 		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
