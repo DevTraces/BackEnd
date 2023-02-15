68afc38783d0bb9a64df827f6fa033aba7e51de0
Dongvin,Park <dongvin99@naver.com>
2023-02-15 15:25:47 +0900

    feat: 최대 팔로우 가능 숫자 5000으로 셋팅(#39)

--- src/main/java/com/devtraces/arterest/common/CommonUtils.java
+++ src/main/java/com/devtraces/arterest/common/CommonUtils.java
@@ -3,5 +3,6 @@ package com.devtraces.arterest.common;
 public class CommonUtils {
 
     public static final int CONTENT_LENGTH_LIMIT = 1000;
+    public static final int FOLLOW_COUNT_LIMIT = 5000;
 
 }
