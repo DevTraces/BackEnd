a3971873b4217ce69345fd8c128431b5acce2f18
heyazoo1007 <heyazoo1007@gmail.com>
2023-02-16 19:41:30 +0900

    test: 비밀번호 변경 테스트 (#41)

--- src/test/java/com/devtraces/arterest/service/user/UserServiceTest.java
+++ src/test/java/com/devtraces/arterest/service/user/UserServiceTest.java
@@ -1,26 +1,35 @@
 package com.devtraces.arterest.service.user;
 
+import com.devtraces.arterest.common.exception.BaseException;
 import com.devtraces.arterest.controller.user.dto.EmailCheckResponse;
 import com.devtraces.arterest.controller.user.dto.NicknameCheckResponse;
 import com.devtraces.arterest.domain.user.User;
 import com.devtraces.arterest.domain.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+import static com.devtraces.arterest.common.exception.ErrorCode.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserService userService;
@@ -67,5 +76,59 @@ class UserServiceTest {
         NicknameCheckResponse response = userService.checkNickname(nickname);
 
         assertEquals(false, response.isDuplicatedNickname());
+    }
+
+    @Test
+    void success_updatePassword() {
+        // given
+        String beforePassword = "beforePassword";
+        String afterPassword = "afterPassword";
+        User user = User.builder().id(1L).password(beforePassword).build();
+
+        given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(anyString(), anyString())).willReturn(true);
+
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+
+        // when
+        userService.updatePassword(1L, beforePassword, afterPassword);
+
+        // then
+        verify(userRepository, times(1)).save(captor.capture());
+        assertEquals(passwordEncoder.encode(afterPassword), captor.getValue().getPassword());
+    }
+
+    @Test
+    void fail_updatePassword_USER_NOT_FOUND() {
+        // given
+        String beforePassword = "beforePassword";
+        String afterPassword = "afterPassword";
+
+        given(userRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // when
+        BaseException exception = assertThrows(BaseException.class,
+                () -> userService.updatePassword(1L, beforePassword, afterPassword));
+
+        // then
+        assertEquals(USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void fail_updatePassword_WRONG_BEFORE_PASSWORD() {
+        // given
+        String beforePassword = "beforePassword";
+        String afterPassword = "afterPassword";
+        User user = User.builder().id(1L).password(beforePassword).build();
+
+        given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(anyString(), anyString())).willReturn(false);
+
+        // when
+        BaseException exception = assertThrows(BaseException.class,
+                () -> userService.updatePassword(1L, beforePassword, afterPassword));
+
+        // then
+        assertEquals(WRONG_BEFORE_PASSWORD, exception.getErrorCode());
     }
 }
