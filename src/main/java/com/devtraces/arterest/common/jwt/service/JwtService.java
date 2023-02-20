a8e944de8b10aaafadac5c8ad788d4b7bb7131c5
gwakjeaha <kjh19920718@gmail.com>
2023-02-21 00:06:41 +0900

    fix: Access Token 내 Bearer 텍스트 분리 로직 추가

--- src/main/java/com/devtraces/arterest/common/jwt/service/JwtService.java
+++ src/main/java/com/devtraces/arterest/common/jwt/service/JwtService.java
@@ -1,5 +1,8 @@
 package com.devtraces.arterest.common.jwt.service;
 
+import static com.devtraces.arterest.common.jwt.JwtProperties.AUTHORIZATION_HEADER;
+import static com.devtraces.arterest.common.jwt.JwtProperties.TOKEN_PREFIX;
+
 import com.devtraces.arterest.common.exception.BaseException;
 import com.devtraces.arterest.common.jwt.JwtProvider;
 import com.devtraces.arterest.common.jwt.dto.TokenDto;
@@ -10,6 +13,7 @@ import com.devtraces.arterest.service.user.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 @Service
@@ -18,10 +22,14 @@ public class JwtService {
 	private final JwtProvider jwtProvider;
 	private final AuthService authService;
 	private final RedisService redisService;
-	private final UserRepository userRepository;
 
 	@Transactional
 	public TokenDto reissue(String accessToken, String refreshToken) {
+
+		if (StringUtils.hasText(accessToken) && accessToken.startsWith(TOKEN_PREFIX)) {
+			accessToken = accessToken.substring(TOKEN_PREFIX.length() + 1);
+		}
+
 		Long userId = Long.parseLong(jwtProvider.getUserId(accessToken));
 		validateTokens(accessToken, refreshToken, userId);
 
