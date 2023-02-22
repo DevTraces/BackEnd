fd8926bc260c5924d474e48f0436e68eeaff5ea3
Dongvin,Park <dongvin99@naver.com>
2023-02-22 10:23:10 +0900

    fix: 반복되는 메서드들 private 메서드로 추출

--- src/main/java/com/devtraces/arterest/service/feed/FeedReadService.java
+++ src/main/java/com/devtraces/arterest/service/feed/FeedReadService.java
@@ -8,7 +8,6 @@ import com.devtraces.arterest.model.feed.FeedRepository;
 import com.devtraces.arterest.model.like.LikeRepository;
 import com.devtraces.arterest.model.like.Likes;
 import com.devtraces.arterest.model.likecache.LikeNumberCacheRepository;
-import com.devtraces.arterest.model.user.User;
 import com.devtraces.arterest.model.user.UserRepository;
 import java.util.List;
 import java.util.Set;
@@ -32,26 +31,13 @@ public class FeedReadService {
 	public List<FeedResponse> getFeedResponseList(
 		Long userId, String nickname, int page, int pageSize
 	){
-		// 요청한 사용자가 좋아요를 누른 피드들의 주키 아이디 번호들을 먼저 불러온다.
-		Set<Long> likedFeedSet = likeRepository.findAllByUserId(userId)
-			.stream().map(Likes::getFeedId).collect(Collectors.toSet());
-
-		// 요청한 사용자가 북마크 했던 피드들의 주키 아이디 번호들도 불러온다.
-		Set<Long> bookmarkedFeedSet = bookmarkRepository.findAllByUserId(userId)
-			.stream().map(bookmark -> bookmark.getFeed().getId()).collect(Collectors.toSet());
-
+		Set<Long> likedFeedSet = getLikedFeedSet(userId);
+		Set<Long> bookmarkedFeedSet = getBookmarkedFeedSet(userId);
 		Long targetUserId = userRepository.findByNickname(nickname)
 			.orElseThrow(() -> BaseException.USER_NOT_FOUND).getId();
-
-		// 피드 별 좋아요 개수는 레디스를 먼저 보게 만들고, 그게 불가능 할때만 Like 테이블에서 찾도록 한다.
 		return feedRepository.findAllByUserId(targetUserId, PageRequest.of(page, pageSize)).stream().map(
 			feed -> {
-				Long likeNumber = likeNumberCacheRepository.getFeedLikeNumber(feed.getId());
-				if(likeNumber == null) {
-					likeNumber = likeRepository.countByFeedId(feed.getId());
-					// 현재 캐시서버에 좋아요 개수가 기록돼 있지 않으므로 다음 read 요쳥을 위해서 캐시해 둠.
-					likeNumberCacheRepository.setLikeNumber(feed.getId(), likeNumber);
-				}
+				Long likeNumber = getOrCacheLikeNumber(feed.getId(), feed);
 				return FeedResponse.from(feed, likedFeedSet, likeNumber, bookmarkedFeedSet);
 			}
 		).collect(Collectors.toList());
@@ -59,22 +45,35 @@ public class FeedReadService {
 
 	@Transactional(readOnly = true)
 	public FeedResponse getOneFeed(Long userId, Long feedId){
-		Set<Long> likedFeedSet = likeRepository.findAllByUserId(userId)
-			.stream().map(Likes::getFeedId).collect(Collectors.toSet());
-
-		Set<Long> bookmarkedFeedSet = bookmarkRepository.findAllByUserId(userId)
-			.stream().map(bookmark -> bookmark.getFeed().getId()).collect(Collectors.toSet());
-
+		Set<Long> likedFeedSet = getLikedFeedSet(userId);
+		Set<Long> bookmarkedFeedSet = getBookmarkedFeedSet(userId);
 		Feed feed = feedRepository.findById(feedId).orElseThrow(() -> BaseException.FEED_NOT_FOUND);
+		Long likeNumber = getOrCacheLikeNumber(feedId, feed);
 
+		return FeedResponse.from(
+			feed, likedFeedSet, likeNumber, bookmarkedFeedSet
+		);
+	}
+
+	// 피드 별 좋아요 개수는 레디스를 먼저 보게 만들고, 그게 불가능 할때만 Like 테이블에서 찾도록 한다.
+	private Long getOrCacheLikeNumber(Long feedId, Feed feed) {
 		Long likeNumber = likeNumberCacheRepository.getFeedLikeNumber(feedId);
 		if(likeNumber == null) {
 			likeNumber = likeRepository.countByFeedId(feedId);
 			likeNumberCacheRepository.setLikeNumber(feed.getId(), likeNumber);
 		}
+		return likeNumber;
+	}
 
-		return FeedResponse.from(
-			feed, likedFeedSet, likeNumber, bookmarkedFeedSet
-		);
+	// 요청한 사용자가 좋아요를 누른 피드들의 주키 아이디 번호들을 먼저 불러온다.
+	private Set<Long> getLikedFeedSet(Long userId) {
+		return likeRepository.findAllByUserId(userId)
+			.stream().map(Likes::getFeedId).collect(Collectors.toSet());
+	}
+
+	// 요청한 사용자가 북마크 했던 피드들의 주키 아이디 번호들도 불러온다.
+	private Set<Long> getBookmarkedFeedSet(Long userId) {
+		return bookmarkRepository.findAllByUserId(userId)
+			.stream().map(bookmark -> bookmark.getFeed().getId()).collect(Collectors.toSet());
 	}
 }
