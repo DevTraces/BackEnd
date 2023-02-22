ff63d2b07ba05375871bf53440a1ee9d94bdabfb
Dongvin,Park <dongvin99@naver.com>
2023-02-22 22:50:45 +0900

    feat: 좋아요 추가 컨트롤러 메서드 정의 (#46)

--- src/main/java/com/devtraces/arterest/controller/like/LikeController.java
+++ src/main/java/com/devtraces/arterest/controller/like/LikeController.java
@@ -1,12 +1,28 @@
 package com.devtraces.arterest.controller.like;
 
+import com.devtraces.arterest.common.response.ApiSuccessResponse;
+import com.devtraces.arterest.service.like.LikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class LikeController {
 
+    private final LikeService likeService;
 
+    @PostMapping("/like/{feedId}")
+    public ApiSuccessResponse<?> createLike(
+        @AuthenticationPrincipal Long userId,
+        @PathVariable Long feedId
+    ){
+        likeService.pressLikeOnFeed(userId, feedId);
+        return ApiSuccessResponse.NO_DATA_RESPONSE;
+    }
 
 }
