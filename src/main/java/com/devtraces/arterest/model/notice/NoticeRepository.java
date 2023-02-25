f6d13177b0b526647918f6131fcb35fcc58ab0a1
heyazoo1007 <heyazoo1007@gmail.com>
2023-02-25 19:32:24 +0900

    feat: 4종류 알림 생성 기능 구현 (#103)

---
+++ src/main/java/com/devtraces/arterest/model/notice/NoticeRepository.java
@@ -0,0 +1,6 @@
+package com.devtraces.arterest.model.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
