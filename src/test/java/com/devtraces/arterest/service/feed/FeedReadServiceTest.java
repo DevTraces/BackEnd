d45a528341bb9cf70055f7935a5928a763818851
Dongvin,Park <dongvin99@naver.com>
2023-02-25 20:54:01 +0900

    fix: feedReadService 테스트코드 수정

--- src/test/java/com/devtraces/arterest/service/feed/FeedReadServiceTest.java
+++ src/test/java/com/devtraces/arterest/service/feed/FeedReadServiceTest.java
@@ -1,11 +1,14 @@
 package com.devtraces.arterest.service.feed;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.devtraces.arterest.common.exception.BaseException;
+import com.devtraces.arterest.common.exception.ErrorCode;
 import com.devtraces.arterest.controller.feed.dto.response.FeedResponse;
 import com.devtraces.arterest.model.bookmark.Bookmark;
 import com.devtraces.arterest.model.bookmark.BookmarkRepository;
@@ -49,7 +52,7 @@ class FeedReadServiceTest {
     @Test
     @DisplayName("피드 리스트 읽기 성공 - 레디스에서 좋아요 개수 획득 성공한 경우.")
     void successGetFeedListRedisServerAvailable(){
-        //given
+        // given
         Reply reply = Reply.builder()
             .id(1L)
             .content("this is reply")
@@ -80,12 +83,12 @@ class FeedReadServiceTest {
         given(feedRepository.findAllByUserIdOrderByCreatedAtDesc(1L, PageRequest.of(0, 10))).willReturn(slice);
         given(userRepository.findByNickname("dongvin99")).willReturn(Optional.of(user));
         given(likeNumberCacheRepository.getFeedLikeNumber(1L)).willReturn(0L);
-        //given(likeRepository.countByFeedId(1L)).willReturn(0L);
+        // given(likeRepository.countByFeedId(1L)).willReturn(0L);
 
-        //when
+        // when
         List<FeedResponse> feedResponseList = feedReadService.getFeedResponseList(1L, "dongvin99", 0, 10);
 
-        //then
+        // then
         verify(likeNumberCacheRepository, times(1)).getFeedLikeNumber(1L);
         verify(feedRepository, times(1)).findAllByUserIdOrderByCreatedAtDesc(1L, PageRequest.of(0, 10));
         assertEquals(feedResponseList.size(), 1);
@@ -94,7 +97,7 @@ class FeedReadServiceTest {
     @Test
     @DisplayName("피드 리스트 읽기 성공 - 레디스에서 좋아요 개수 획득에 실패한 경우.")
     void successGetFeedListRedisServerNotAvailable(){
-        //given
+        // given
         Reply reply = Reply.builder()
             .id(1L)
             .content("this is reply")
@@ -128,10 +131,10 @@ class FeedReadServiceTest {
         given(likeRepository.countByFeedId(1L)).willReturn(0L);
         doNothing().when(likeNumberCacheRepository).setLikeNumber(1L, 0L);
 
-        //when
+        // when
         List<FeedResponse> feedResponseList = feedReadService.getFeedResponseList(1L, "dongvin99", 0, 10);
 
-        //then
+        // then
         verify(likeNumberCacheRepository, times(1)).getFeedLikeNumber(1L);
         verify(likeRepository, times(1)).countByFeedId(1L);
         verify(likeNumberCacheRepository, times(1)).setLikeNumber(1L, 0L);
@@ -142,7 +145,7 @@ class FeedReadServiceTest {
     @Test
     @DisplayName("피드 1개 읽기 성공 - 레디스에서 좋아요 개수 획득에 성공한 경우.")
     void successGetOneFeedRedisServerAvailable(){
-        //given
+        // given
         Reply reply = Reply.builder()
             .id(1L)
             .content("this is reply")
@@ -173,10 +176,10 @@ class FeedReadServiceTest {
         given(feedRepository.findById(1L)).willReturn(Optional.of(feed));
         given(likeNumberCacheRepository.getFeedLikeNumber(1L)).willReturn(0L);
 
-        //when
+        // when
         FeedResponse feedResponse = feedReadService.getOneFeed(1L, 1L);
 
-        //then
+        // then
         verify(likeRepository, times(1)).findAllByUserId(1L);
         verify(bookmarkRepository, times(1)).findAllByUserId(1L);
         verify(likeNumberCacheRepository, times(1)).getFeedLikeNumber(1L);
@@ -187,7 +190,7 @@ class FeedReadServiceTest {
     @Test
     @DisplayName("피드 1개 읽기 성공 - 레디스에서 좋아요 개수 획득에 실패한 경우")
     void successGetOneFeedRedisServerNotAvailable(){
-        //given
+        // given
         Reply reply = Reply.builder()
             .id(1L)
             .content("this is reply")
@@ -220,10 +223,10 @@ class FeedReadServiceTest {
         given(likeRepository.countByFeedId(1L)).willReturn(0L);
         doNothing().when(likeNumberCacheRepository).setLikeNumber(1L, 0L);
 
-        //when
+        // when
         FeedResponse feedResponse = feedReadService.getOneFeed(1L, 1L);
 
-        //then
+        // then
         verify(likeRepository, times(1)).findAllByUserId(1L);
         verify(bookmarkRepository, times(1)).findAllByUserId(1L);
         verify(likeNumberCacheRepository, times(1)).getFeedLikeNumber(1L);
@@ -231,6 +234,43 @@ class FeedReadServiceTest {
         verify(likeNumberCacheRepository, times(1)).setLikeNumber(1L, 0L);
         verify(feedRepository, times(1)).findById(1L);
         assertEquals(feedResponse.getFeedId(), 1L);
+    }
+
+    @Test
+    @DisplayName("게시물 엔티티 1개 찾기 성공")
+    void successGetOneFeedEntity(){
+        // given
+        Feed feed = Feed.builder()
+            .id(1L)
+            .build();
+
+        given(feedRepository.findById(anyLong())).willReturn(Optional.of(feed));
+
+        // when
+        feedReadService.getOneFeedEntity(1L);
+
+        // then
+        verify(feedRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("게시물 엔티티 1개 찾기 실패 - 게시물이 존재하지 않음")
+    void failedGetOneFeedEntityFeedNotFound(){
+        // given
+        Feed feed = Feed.builder()
+            .id(1L)
+            .build();
+
+        given(feedRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // when
+        BaseException exception = assertThrows(
+            BaseException.class ,
+            () -> feedReadService.getOneFeedEntity(1L)
+        );
+
+        // then
+        assertEquals(ErrorCode.FEED_NOT_FOUND, exception.getErrorCode());
     }
 
 }
