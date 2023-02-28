cb322647942a4cf4aa461f7fb481a7878f7a55d3
Dongvin,Park <dongvin99@naver.com>
2023-02-28 16:18:37 +0900

    fix: LikeResponse 의 필드를 수정함 (#118)

--- src/main/java/com/devtraces/arterest/controller/like/dto/response/LikeResponse.java
+++ src/main/java/com/devtraces/arterest/controller/like/dto/response/LikeResponse.java
@@ -18,25 +18,16 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LikeResponse {
 
-    private Long userId;
     private String profileImageLink;
     private String userName;
     private String nickname;
-    private String description;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
-    private UserSignUpType signupType;
+    private boolean isFollowing;
 
     public static LikeResponse from(User user){
         return LikeResponse.builder()
-            .userId(user.getId())
             .profileImageLink(user.getProfileImageUrl())
             .userName(user.getUsername())
             .nickname(user.getNickname())
-            .description(user.getDescription())
-            .createdAt(user.getCreatedAt())
-            .modifiedAt(user.getModifiedAt())
-            .signupType(user.getSignupType())
             .build();
     }
 
